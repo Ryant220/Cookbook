3e6adc52b8650b2f0632287442009dc13d9100f4
Ryant220 <rtaylor-483@hotmail.com>
2024-03-13 11:49:46 -0500

    up and running

---
+++ backend/index.js
@@ -0,0 +1,51 @@
+import express from "express"
+import mysql from "mysql"
+
+const app = express()
+
+
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"Overkill-483",
+    database: "cookbook"
+})
+
+
+app.use(express.json())
+
+app.get("/", (req,res)=>{
+    res.json("hello this is the backend")
+})
+
+app.get("/recipes", (req,res)=>{
+    const q = "SELECT * from recipes"
+    db.query(q,(err,data) => {
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+})
+
+app.post("/recipes", (req, res) => {
+    const q = "INSERT INTO recipes(`title`, `desc`, `author`, `picture`) VALUES (?)";
+
+    const values = [
+      req.body.title,
+      req.body.desc,
+      req.body.author,
+      req.body.picture,
+    ]
+
+    // const values = ["title from backend", "desc from backend", "Ryan", "picture from backend"]
+
+
+    db.query(q, [values], (err, data) => {
+        if (err) return res.send(err);
+        return res.json(data);
+      });
+})
+
+
+app.listen(8800, ()=>{
+    console.log("Connexted to backend")
+})
---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      Cookbook
+    </div>
+  );
+}
+
+export default App;
