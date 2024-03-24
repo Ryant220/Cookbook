2bdb58bf2e3d2e304e8c39bbee4fafa3f2eb02be
Ryant220 <rtaylor-483@hotmail.com>
2024-03-23 22:08:41 -0500

    testing public picture url

--- client/src/components/Recipe.jsx
+++ client/src/components/Recipe.jsx
@@ -4,7 +4,7 @@ import ChickenPicture from '../pictures/ChickenPicture.jpg'
 function Recipe(props) {
     return (
         <div>
-            <img src={ChickenPicture} />
+            <img src="https://publiccookbookbucket.s3.amazonaws.com/ChickenPicture.jpg" />
             <h1>{props.title}</h1>
             <h2>{props.author}</h2>
             <p>{props.desc}</p>
