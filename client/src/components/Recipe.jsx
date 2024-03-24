ce1b1b4aab1603695bf25d6ec0a25bae02a749d6
Ryant220 <rtaylor-483@hotmail.com>
2024-03-23 21:08:18 -0500

    Figuring out img

--- client/src/components/Recipe.jsx
+++ client/src/components/Recipe.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import ChickenPicture from '../pictures/ChickenPicture.jpg'
 
 function Recipe(props) {
     return (
         <div>
+            <img src={ChickenPicture} />
             <h1>{props.title}</h1>
             <h2>{props.author}</h2>
             <p>{props.desc}</p>
